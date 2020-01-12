9d27cf6a4fb47dc955693c116671c4387fd59435
Shilpa Kamble <kambleashilpa@gmail.com>
2020-01-12 10:24:58 +0530

    Added IsAdminPageLoaded method

--- git-automation-framework/src/main/java/com/orangehrm/git/pages/GitAdminPage.java
+++ git-automation-framework/src/main/java/com/orangehrm/git/pages/GitAdminPage.java
@@ -2,6 +2,11 @@ package com.orangehrm.git.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.orangehram.git.common.GitSeleniumWebDriverFactory;
 
 
 public class GitAdminPage {
@@ -11,6 +16,12 @@ public class GitAdminPage {
 
 	@FindBy(id = "menu_admin_UserManagement")
 	WebElement userManagementTab;
+	WebDriverWait wait = new WebDriverWait(GitSeleniumWebDriverFactory.getDriver(),30);
+	
+	public GitAdminPage()
+	{
+		PageFactory.initElements(GitSeleniumWebDriverFactory.getDriver(), this);
+	}
 	
 	public GitAddUserPage clickOnAddUserBtn() {
 		userManagementTab.click();
@@ -18,4 +29,10 @@ public class GitAdminPage {
 		return new GitAddUserPage();
 	}
 
+	public GitAdminPage isAdminPageLoaded()
+	{
+		wait.until(ExpectedConditions.elementToBeClickable(userManagementTab));
+		wait.until(ExpectedConditions.elementToBeClickable(addUserBtn));
+		return this;
+	}
 }
